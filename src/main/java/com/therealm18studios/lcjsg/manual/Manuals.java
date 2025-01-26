fbf85117024048753c9dfddf522a71967bba8e03
Robert MacRae <git@therealm18.com>
2025-01-26 15:01:01 -0400

    more clean-up

--- src/main/java/com/therealm18studios/lcjsg/manual/Manuals.java
+++ src/main/java/com/therealm18studios/lcjsg/manual/Manuals.java
@@ -5,8 +5,6 @@ import com.therealm18studios.lcjsg.Items;
 import li.cil.manual.api.ManualModel;
 import li.cil.manual.api.Tab;
 import li.cil.manual.api.prefab.Manual;
-import li.cil.manual.api.prefab.provider.NamespaceDocumentProvider;
-import li.cil.manual.api.prefab.provider.NamespacePathProvider;
 import li.cil.manual.api.prefab.tab.ItemStackTab;
 import li.cil.manual.api.provider.DocumentProvider;
 import li.cil.manual.api.provider.PathProvider;
@@ -39,9 +37,6 @@ public final class Manuals {
         PATH_PROVIDERS.register(FMLJavaModLoadingContext.get().getModEventBus());
         CONTENT_PROVIDERS.register(FMLJavaModLoadingContext.get().getModEventBus());
         TABS.register(FMLJavaModLoadingContext.get().getModEventBus());
-
-//        PATH_PROVIDERS.register("path_provider", () -> new NamespacePathProvider(LCJSG.MOD_ID));
-//        CONTENT_PROVIDERS.register("content_provider", () -> new NamespaceDocumentProvider(LCJSG.MOD_ID, "doc"));
 
         TABS.register("lcjsg", () -> new ItemStackTab(
             ManualModel.LANGUAGE_KEY + "/lcjsg/index.md",
